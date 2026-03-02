ae7f1737f501e1a6fca1986c2a24d843cb0328a0
Bartosz Godzieszka <bartosz.godzieszka@o2.pl>
2026-03-02 11:23:31 +0100

    add data fetching

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,32 @@
-
+import 'react';
+import { useState, useEffect } from 'react';
 //fetch, memory links to other pages,
 //header dosent change, only the content
 
 
-const App = () => <h1>Our First Test</h1>;
+
+
+function App(){
+    const [storeItems, setStoreItems] = useState([]);
+    const [cartItems, setCatrtItems] = useState([]);
+
+    //Fetching data from the server
+    useEffect(() => {
+        fetch('https://fakestoreapi.com/products')
+            .then(response => {
+                if(response.status >= 400){
+                    throw new Error(`Failed to fetch data, status code: ${response.status}`);
+                }
+                return response.json();
+            })
+            .then(data => setStoreItems(data))
+            .catch(e => console.error(`Error fetching data: ${e.message}`));
+        
+    },[]);
+
+    console.log(storeItems)
+
+    return <h1>Our Firsts Test</h1>
+}
 
 export default App;
