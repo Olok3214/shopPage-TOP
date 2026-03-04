bcd63faa7d18f0a83439fc55f83efe67b3743513
Bartosz Godzieszka <bartosz.godzieszka@o2.pl>
2026-03-04 11:59:50 +0100

    add cart funcionality

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -1,10 +1,28 @@
 import { useOutletContext } from "react-router";
+import CartPanel from './CartPanel'
+import { useMemo } from "react";
 
 function BasketPage(){
     const {cartItems , setCartItems} = useOutletContext()
-    
-    console.log(cartItems)
 
-    return <h1> basket page</h1>
+    const total = useMemo(() => cartItems.reduce((sum, item) => sum + (item.price * item.selectedAmount), 0), [cartItems])
+
+
+//title,price,selectedAmount, setCartItems
+    return (<>
+        <h1> Your Cart:</h1>
+        {cartItems.map( item => <CartPanel 
+            key = {item.itemId}
+            itemId={item.itemId}
+            price = {item.price}
+            selectedAmount = {item.selectedAmount}
+            setCartItems = {setCartItems}
+        />
+        )}
+        <div className="cartTotal">
+            Total: ${total.toFixed(2)}
+            <button type="button">Check out</button>
+        </div>
+    </>)
 }
 export default BasketPage;
--- src/pages/CartPanel.jsx
+++ src/pages/CartPanel.jsx
@@ -1,13 +1,20 @@
 
-function CartPanel({title,price,selectedAmount, setCartItems}){
+function CartPanel({itemId,title,price,selectedAmount, setCartItems}){
 
+    function handleRemoveItem(){
+        setCartItems(prevItems => prevItems.filter(item => item.itemId !== itemId))
+    }
 
     return (
         <div className="cartPanel">
             <h2>{title}</h2>
-
+            <p>
+                Price: ${price} Amount: {selectedAmount}
+            </p>
+            <p>Total: ${(price * selectedAmount).toFixed(2)}</p>
+            <button type="button" onClick={handleRemoveItem}> Remove </button>
         </div>
     )
-        
-    
 }
+
+export default CartPanel
